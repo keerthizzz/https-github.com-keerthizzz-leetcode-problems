ee6362ce6c6f0574802d5f81a6eb46f483e86041
keerthizzz <keerthizzz142@gmail.com>
2026-07-22 22:03:03 +0530

    Time: 0 ms (100%), Space: 19.1 MB (91.81%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        n = len(num)
+        
+        for i in range(1, n):
+            for j in range(i+1, n):
+                first, second = num[:i], num[i:j]
+                
+                if (len(first) > 1 and first[0] == '0') or (len(second) > 1 and second[0] == '0'):
+                    continue
+                
+                a, b = int(first), int(second)
+                k = j
+                while k < n:
+                    c = a + b
+                    c_str = str(c)
+                    if not num.startswith(c_str, k):
+                        break
+                    k += len(c_str)
+                    a, b = b, c
+                
+                if k == n:  
+                    return True
+        
+        return False
