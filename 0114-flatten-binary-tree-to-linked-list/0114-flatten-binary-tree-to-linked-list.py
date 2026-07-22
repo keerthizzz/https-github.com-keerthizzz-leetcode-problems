f8f40c48a0169b52dacfa4dba6aa0d8bca53e36f
keerthizzz <keerthizzz142@gmail.com>
2026-07-22 21:43:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.57%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        def helper(node: Optional[TreeNode]) -> Optional[TreeNode]:
+            if not node:
+                return None
+            if not node.left and not node.right:
+                return node
+            left_tail = helper(node.left)
+            right_tail = helper(node.right)
+            if left_tail:
+                left_tail.right = node.right
+                node.right = node.left
+                node.left = None
+            return right_tail if right_tail else left_tail
+        
+        helper(root)
