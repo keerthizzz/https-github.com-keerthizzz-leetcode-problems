3abe9ccaa9c7581da928dfc32b78cf703dfb7677
keerthizzz <keerthizzz142@gmail.com>
2026-07-22 22:03:29 +0530

    Time: 357 ms (85.05%), Space: 38.2 MB (75.47%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -0,0 +1,29 @@
+class NumArray:
+
+    def __init__(self, nums: list[int]):
+        self.n = len(nums)
+        self.nums = nums[:]  
+        self.bit = [0] * (self.n + 1)  
+        
+        for i, val in enumerate(nums):
+            self._add(i+1, val)
+
+    def _add(self, i: int, delta: int):
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
+
+    def _prefix_sum(self, i: int) -> int:
+        s = 0
+        while i > 0:
+            s += self.bit[i]
+            i -= i & -i
+        return s
+
+    def update(self, index: int, val: int) -> None:
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        self._add(index+1, delta)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self._prefix_sum(right+1) - self._prefix_sum(left)
