efef2ee9b52a565b63d5a19ad92d5829b9156c6b
keerthizzz <keerthizzz142@gmail.com>
2026-07-22 22:01:37 +0530

    Time: 1 ms (93.12%), Space: 19.3 MB (31.29%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls = 0
+        cows = 0
+        
+        secret_count = [0] * 10
+        guess_count = [0] * 10
+        
+        for s, g in zip(secret, guess):
+            if s == g:
+                bulls += 1
+            else:
+                secret_count[int(s)] += 1
+                guess_count[int(g)] += 1
+        
+        for d in range(10):
+            cows += min(secret_count[d], guess_count[d])
+        
+        return f"{bulls}A{cows}B"
