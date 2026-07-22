ef2f5c1a3f7f98e464be7c503165ee798eaa04da
keerthizzz <keerthizzz142@gmail.com>
2026-07-22 22:13:38 +0530

    Time: 4 ms (28.87%), Space: 19.3 MB (50.13%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans = []
+
+        for i in arr2:
+            while i in arr1:
+                ans.append(i)
+                arr1.remove(i)
+
+        arr1.sort()
+        return ans + arr1
