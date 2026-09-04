e665abc6824e1632782575f7563c60a8850606d3
keerthizzz <keerthizzz142@gmail.com>
2026-09-04 13:59:35 +0530

    Time: 11 ms (68.97%), Space: 20.6 MB (53.94%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        count = [0] * (n + 1)
+
+        for x in nums:
+            count[x] += 1
+
+        duplicate = 0
+        missing = 0
+
+        for i in range(1, n + 1):
+            if count[i] == 2:
+                duplicate = i
+            elif count[i] == 0:
+                missing = i
+
+        return [duplicate, missing]
+        
